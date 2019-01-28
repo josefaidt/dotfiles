1d92fd28f589d8e719358294c210992120f5df01
josefaidt <josef.aidt@gmail.com>
2019-01-28 10:15:38 -0600

    add ranger, anaconda

--- setup.sh
+++ setup.sh
@@ -2,6 +2,12 @@
 
 echo 'Setting up your new device...'
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `setup.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -10,7 +16,8 @@ brew install \
   fish \
   node \
   yarn \
-  tmux
+  tmux \
+  ranger
 
 # change default shell to /usr/local/bin/fish
 # install Fisher and Bass (for nvm, if we need it later)
@@ -32,7 +39,8 @@ brew cask install \
   github \
   vlc \
   postman \
-  graphql-playground
+  graphql-playground \
+  anaconda
 
 # change default shell to fish
 sudo echo /usr/local/bin/fish >> /etc/shells
