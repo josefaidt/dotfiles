8152c7ca2a6ad40d7667550ba47be111f33e75d9
josefaidt <josef.aidt@gmail.com>
2025-11-26 10:44:10 -0800

    clean up, start reorganizing

--- .config/nvim/lua/plugins/sessions.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"rmagatti/auto-session",
-	lazy = false,
-	opts = {
-		auto_restore_enabled = true,
-		auto_save_enabled = true,
-		suppressed_dirs = { "~/", "~/Downloads", "/" },
-	},
-}
