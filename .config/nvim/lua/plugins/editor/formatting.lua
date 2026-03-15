3500f904bf48c46025cfa7775d2ae23edddd4ed3
josefaidt <josef.aidt@gmail.com>
2026-03-15 15:20:52 -0700

    lift conform linter errors

--- .config/nvim/lua/plugins/editor/formatting.lua
+++ .config/nvim/lua/plugins/editor/formatting.lua
@@ -10,6 +10,32 @@ return {
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
 	-- Keymaps are configured in lua/config/keymaps/plugins.lua
+	init = function()
+		local disable_filetypes = { c = true, cpp = true }
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			callback = function(args)
+				if disable_filetypes[vim.bo[args.buf].filetype] then
+					return
+				end
+				local done = false
+				local format_err = nil
+				require("conform").format({
+					bufnr = args.buf,
+					timeout_ms = 2000,
+					lsp_format = "fallback",
+				}, function(err, _did_edit)
+					format_err = err
+					done = true
+				end)
+				vim.wait(2500, function()
+					return done
+				end, 10)
+				if format_err then
+					vim.notify(format_err, vim.log.levels.ERROR, { title = "conform.nvim" })
+				end
+			end,
+		})
+	end,
 	opts = function()
 		local function has_biome_config()
 			local root = vim.fs.dirname(vim.fs.find({ "biome.json", "biome.jsonc" }, { upward = true })[1])
@@ -22,21 +48,7 @@ return {
 		end
 
 		return {
-			notify_on_error = true,
-			format_on_save = function(bufnr)
-				-- Disable "format_on_save lsp_fallback" for languages that don't
-				-- have a well standardized coding style. You can add additional
-				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
-				if disable_filetypes[vim.bo[bufnr].filetype] then
-					return nil
-				else
-					return {
-						timeout_ms = 2000,
-						lsp_format = "fallback",
-					}
-				end
-			end,
+			notify_on_error = false,
 			formatters_by_ft = {
 				lua = { "stylua" },
 				rust = { "rustfmt", lsp_format = "fallback" },
