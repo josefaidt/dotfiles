865172ef9c80695a9c15588ac57c4cf2b46659b8
josefaidt <josef.aidt@gmail.com>
2026-05-02 09:37:04 -0700

    gate loadenv with interactive shell

--- .config/fish/functions/cd.fish
+++ .config/fish/functions/cd.fish
@@ -1,5 +1,7 @@
 # override cd to load environment variables from dotenv files
 function cd
   builtin cd $argv
-  loadenv --silent; or true
+  if status is-interactive
+    loadenv --silent; or true
+  end
 end
