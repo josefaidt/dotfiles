5f19fb845444cf538a056a8df6ad0dd61e333d4d
josefaidt <josef.aidt@gmail.com>
2026-08-01 08:18:24 -0700

    minimal statusline layout

--- .config/nvim/lua/plugins/ui/statusline.lua
+++ .config/nvim/lua/plugins/ui/statusline.lua
@@ -22,7 +22,29 @@ return {
 		require("mini.surround").setup()
 
 		local statusline = require("mini.statusline")
-		statusline.setup({ use_icons = vim.g.have_nerd_font })
+		statusline.setup({
+			use_icons = vim.g.have_nerd_font,
+			content = {
+				-- Minimal layout: mode (single letter) | git | diagnostics | filename … location
+				-- Deliberately omits the fileinfo block (encoding/format/filetype).
+				active = function()
+					local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+					local git = statusline.section_git({ trunc_width = 40 })
+					local diagnostics = statusline.section_diagnostics({ trunc_width = 75 })
+					local filename = statusline.section_filename({ trunc_width = 140 })
+					local location = statusline.section_location({ trunc_width = 75 })
+
+					return statusline.combine_groups({
+						{ hl = mode_hl, strings = { mode:sub(1, 1):upper() } },
+						{ hl = "MiniStatuslineDevinfo", strings = { git, diagnostics } },
+						"%<", -- truncate from here if the line is too long
+						{ hl = "MiniStatuslineFilename", strings = { filename } },
+						"%=", -- right-align what follows
+						{ hl = mode_hl, strings = { location } },
+					})
+				end,
+			},
+		})
 
 		---@diagnostic disable-next-line: duplicate-set-field
 		statusline.section_location = function()
